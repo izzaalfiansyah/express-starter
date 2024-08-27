8742c8e788a128a74a1436ddd20cb896ab6f2106
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2024-08-27 16:04:37 +0700

    try using manual routing

--- src/utils/router.ts
+++ src/utils/router.ts
@@ -1,23 +1,30 @@
 import { Router, type Express } from "express";
-import path from "path";
-import fs from "fs";
-
-export const handleRouter = (app: Express) => {
-  const routesDir = path.join(__dirname, "../routes");
-  const routesFiles = fs.readdirSync(routesDir);
-
-  routesFiles.forEach((file) => {
-    const router: Router = require(path.join(routesDir, file)).default;
-    const filePath = file.includes("index.ts")
-      ? "/"
-      : `/${file.replace(".ts", "")}`;
-
-    app.use(filePath, router);
-  });
-};
+import helloControler from "../controller/hello-controler";
+// import path from "path";
+// import fs from "fs";
 
 export const useRouter = () => {
   const router = Router();
 
   return router;
+};
+
+export const handleRouter = (app: Express) => {
+  // const routesDir = path.join(__dirname, "../routes");
+  // const routesFiles = fs.readdirSync(routesDir);
+
+  // routesFiles.forEach((file) => {
+  //   const router: Router = require(path.join(routesDir, file)).default;
+  //   const filePath = file.includes("index.ts")
+  //     ? "/"
+  //     : `/${file.replace(".ts", "")}`;
+
+  //   app.use(filePath, router);
+  // });
+
+  app.get("/", (req, res) => {
+    return res.send("It's Worked!");
+  });
+
+  app.get("/hello", helloControler.get);
 };
