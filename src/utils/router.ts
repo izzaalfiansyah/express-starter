ddbe0990c6cc1a964ee24b3c40b24bfdab17fd2f
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2024-08-27 15:06:09 +0700

    make router util

---
+++ src/utils/router.ts
@@ -0,0 +1,9 @@
+import { Router, type Express } from "express";
+
+export const handleRouter = (app: Express) => {};
+
+export const useRouter = () => {
+  const router = Router();
+
+  return router;
+};
