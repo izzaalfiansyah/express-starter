8f5ab215a227e91b511abb20bb5754667dcfc120
Muhammad Izza Alfiansyah <iansyah724@gmail.com>
2024-08-27 15:39:07 +0700

    add cors and .env.example

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import cors from "cors";
 import "dotenv/config";
 import { handleRouter } from "./utils/router";
 
 const app = express();
+app.use(express.json());
+app.use(cors());
+
 const port = process.env.PORT || 3000;
 
 handleRouter(app);
